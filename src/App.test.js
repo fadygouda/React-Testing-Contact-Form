1ea7189f57130bc7180c161fec4640072baf8dd9
fadygouda <fadygouda123@gmail.com>
2020-05-14 20:18:20 -0700

    mvp complete

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,37 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, fireEvent } from "@testing-library/react";
 import App from "./App";
+import ContactForm from './components/ContactForm'
 
-test("renders App without crashing", () => {
-  render(<App />);
+test("check input First name, last name, email and message without crashing", () => {
+  const { getByText } = render(<App />);
+
+  const firstName = getByText(/first name/i);
+  const lastName = getByText(/last name/i);
+  const email = getByText(/email/i);
+  const message = getByText(/message/i);
+
+  expect(firstName).toBeInTheDocument();
+  expect(lastName).toBeInTheDocument();
+  expect(email).toBeInTheDocument();
+  expect(message).toBeInTheDocument();
 });
+
+test("the functionality of the signup form", () => {
+  const { getByLabelText } = render(<ContactForm />);
+  fireEvent.change(getByLabelText(/first name/i), {
+  target: { value: "Fady"}
+});
+  fireEvent.change(getByLabelText(/last name/i), {
+    target: { value: "Gouda"}
+  });
+  fireEvent.change(getByLabelText(/email/i), {
+    target: { value: "fadygouda123@gmail.com"}
+  });
+  fireEvent.change(getByLabelText(/message/i), {
+    target: { value: "This is my first actual test using Jest!"}
+  });
+
+
+});
+
--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -14,8 +14,9 @@ const ContactForm = () => {
     <div className="App">
       <form onSubmit={handleSubmit(onSubmit)}>
         <div>
-          <label htmlFor="firstName">First Name*</label>
+          <label htmlFor="firstName">First Name</label>
           <input
+            id="firstName"
             name="firstName"
             placeholder="Edd"
             ref={register({ required: true, maxLength: 3 })}
@@ -26,8 +27,9 @@ const ContactForm = () => {
         </div>
 
         <div>
-          <label htmlFor="lastName">Last Name*</label>
+          <label htmlFor="lastName">Last Name</label>
           <input
+            id="lastName"
             name="lastName"
             placeholder="Burke"
             ref={register({ required: true })}
@@ -41,14 +43,14 @@ const ContactForm = () => {
           <label htmlFor="email" placeholder="bluebill1049@hotmail.com">
             Email*
           </label>
-          <input name="email" ref={register({ required: true })} />
+          <input id="email" name="email" ref={register({ required: true })} />
           {errors.email && (
             <p>Looks like there was an error: {errors.email.type}</p>
           )}
         </div>
         <div>
           <label htmlFor="message">Message</label>
-          <textarea name="message" ref={register({ required: false })} />
+          <textarea id="message" name="message" ref={register({ required: false })} />
         </div>
         {data && (
           <pre style={{ textAlign: "left", color: "white" }}>
